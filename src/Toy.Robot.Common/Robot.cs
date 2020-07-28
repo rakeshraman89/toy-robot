d795e5d370298dd73c53ef1a122d3823f95df813
Rakesh Raman Unni <rakeshshyam.123@gmail.com>
2020-07-29 00:32:38 +1000

    Console app input statements

---
+++ src/Toy.Robot.Common/Robot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Toy.Robot.Common
+{
+    public class Robot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Direction { get; set; }
+    }
+}
--- src/Toy.Robot.ConsoleApp/Program.cs
+++ src/Toy.Robot.ConsoleApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
 
 namespace Toy.Robot.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**************");
+            Console.WriteLine("\nHi this is Chitti-The robot!");
+            Console.WriteLine("\nEnter the file name:");
+            var fileName = Console.ReadLine();
+            var lines = File.ReadAllLines($"C:/Dev/Source/Sample/toy-robot-puzzle/TestData/{fileName}.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
         }
     }
 }
