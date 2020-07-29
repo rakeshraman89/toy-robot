e616cddb03dc97c57ccb2a47df6927631d14b3a4
Rakesh Raman Unni <rakeshshyam.123@gmail.com>
2020-07-30 00:45:58 +1000

    added implementation for command operations

---
+++ src/Toy.Robot.Common/Utils/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toy.Robot.Common.Utils
+{
+    public enum FacingDirection
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public enum Commands
+    {
+        PLACE,
+        MOVE,
+        LEFT,
+        RIGHT,
+        REPORT
+    }
+}
---
+++ src/Toy.Robot.Common/Utils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toy.Robot.Common.Utils
+{
+    public static class Extensions
+    {
+        public static bool IsPlacementValid(this Robot robot, TableTop board)
+        {
+            return 0 <= robot.Coordinate.X
+                   && robot.Coordinate.X <= board.Length
+                   && 0 <= robot.Coordinate.Y
+                   && robot.Coordinate.Y <= board.Breadth;
+        }
+    }
+}
